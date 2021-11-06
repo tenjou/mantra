63d66d140166b8d9d122cafbecb7a1d03175de0f
Arturs Šefers <sefers.arturs@gmail.com>
2021-11-06 21:27:56 +0200

    Add support for identifier availability checking

--- src/analyzer.js
+++ src/analyzer.js
@@ -2,11 +2,15 @@ import fs from "fs"
 import path from "path"
 
 function handleFunctionDeclaration(ctx, node) {
-    const func = declareFunc(ctx, ctx.scope, node.id)
+    const func = declareFunc(ctx, node.id)
+    const scopePrev = ctx.scopeCurr
+    ctx.scopeCurr = func.scope
 
     for (const param of node.params) {
         declareVar(ctx, func.scope, param)
     }
+
+    ctx.scopeCurr = scopePrev
 }
 
 function handleImportDeclaration(ctx, node) {
@@ -19,47 +23,67 @@ function handleImportDeclaration(ctx, node) {
     }
 }
 
+function handleIfStatement(ctx, node) {
+    handle[node.test.kind](ctx, node.test)
+}
+
+function handleBinaryExpression(ctx, node) {
+    handle[node.left.kind](ctx, node.left)
+    handle[node.right.kind](ctx, node.right)
+}
+
 function handleBody(ctx, body) {
     for (const node of body) {
         handle[node.kind](ctx, node)
     }
 }
 
-export function analyze({ program, input, fileName }) {
-    const ctx = {
-        input,
-        fileName,
-        scope: createScope(),
+function handleIdentifier(ctx, node) {
+    if (!exists(ctx, node.name)) {
+        raise(ctx, node, `Cannot find name '${node.name}'`)
     }
-
-    handleBody(ctx, program.body)
 }
 
-function declareFunc(ctx, scope, node) {
-    if (scope.vars[node.name]) {
+function handleNoop(_ctx, _node) {}
+
+function exists(ctx, name) {
+    let scope = ctx.scopeCurr
+    while (scope) {
+        if (scope.vars[name]) {
+            return true
+        }
+        scope = scope.parent
+    }
+
+    return false
+}
+
+function declareFunc(ctx, node) {
+    if (exists(ctx, node.name)) {
         raise(ctx, "Duplicate function implementation", node)
     }
 
     const newVar = createVar()
-    newVar.scope = createScope()
-    scope.vars[node.name] = newVar
+    newVar.scope = createScope(ctx.scope)
+    ctx.scopeCurr.vars[node.name] = newVar
 
     return newVar
 }
 
-function declareVar(ctx, scope, node) {
-    if (scope.vars[node.name]) {
+function declareVar(ctx, node) {
+    if (exists(node.name)) {
         raise(ctx, node, `Duplicate identifier '${node.name}'`)
     }
 
     const newVar = createVar()
-    scope.vars[node.name] = newVar
+    ctx.scopeCurr.vars[node.name] = newVar
 
     return newVar
 }
 
-function createScope() {
+function createScope(parent) {
     return {
+        parent,
         vars: {},
     }
 }
@@ -74,7 +98,23 @@ function raise(ctx, node, error) {
     throw new SyntaxError(`${error}. ${ctx.fileName}:${1}:${node.start + 1}`)
 }
 
+export function analyze({ program, input, fileName }) {
+    const ctx = {
+        input,
+        fileName,
+        scope: createScope(null),
+        scopeCurr: null,
+    }
+
+    handleBody(ctx, program.body)
+}
+
 const handle = {
     FunctionDeclaration: handleFunctionDeclaration,
     ImportDeclaration: handleImportDeclaration,
+    IfStatement: handleIfStatement,
+    BinaryExpression: handleBinaryExpression,
+    Identifier: handleIdentifier,
+    Literal: handleNoop,
+    NumericLiteral: handleNoop,
 }
